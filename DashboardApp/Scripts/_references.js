e448ed17e41359ef13e1610fb9fd8181d97ecbba
marek331 <marek.tooming@treasuryview.com>
2017-08-16 14:15:32 +0300

    Added StyleBundles and Scriptbundles.

--- DashboardApp/Scripts/_references.js
+++ DashboardApp/Scripts/_references.js
@@ -2,4 +2,4 @@
 /// <reference path="jquery-3.1.1.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
-/// <reference path="knockout-2.2.0.debug.js" />
+// <reference path="knockout-2.2.0.debug.js" />
